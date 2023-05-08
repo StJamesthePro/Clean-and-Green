6bfb50dc52fa476861ca588aaffcc94717b72a83
Julian StJames <julianstjames@gmail.com>
2023-05-08 11:27:10 -0400

    adds accordian feature automates new panel feature

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const button5 = document.getElementById('search_button');
 
 
 
+// Add functionality to toggle the accordion panel when the button is clicked
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+
 // Add a click event listener to the buttons
 button1.addEventListener('click', function () {
     // Code to execute when the button is clicked
@@ -32,20 +49,63 @@ button5.addEventListener('click', function () {
 
 //ADDING INFORMATION TO THE SCROLL LIST
 // Create a new HTML element
-const newElement = document.createElement('p');
-const newHeading = document.createElement('h4');
-const newElement2 = document.createElement('p');
-const newHeading2 = document.createElement('h4');
-newHeading.textContent = 'This is a New Header';
-newElement.textContent = 'This is some new content!';
-newHeading2.textContent = 'This is another New Header';
-newElement2.textContent = 'This is some more new content!';
+// const newElement = document.createElement('p');
+// const newHeading = document.createElement('h4');
+// const newElement2 = document.createElement('p');
+// const newHeading2 = document.createElement('h4');
+// newHeading.textContent = 'This is a New Header';
+// newElement.textContent = 'This is some new content!';
+// newHeading2.textContent = 'This is another New Header';
+// newElement2.textContent = 'This is some more new content!';
 
 // Find the container where you want to append the new element
 const container = document.querySelector('.scrollspy-example');
 
 // Append the new element to the container
-container.appendChild(newHeading);
-container.appendChild(newElement);
-container.appendChild(newHeading2);
-container.appendChild(newElement2);
+// container.appendChild(newHeading);
+// container.appendChild(newElement);
+// container.appendChild(newHeading2);
+// container.appendChild(newElement2);
+
+
+// Dynamically add a new panel when new data is added
+function addPanel(title, content) {
+    // Create a new button and panel
+    var button = document.createElement("button");
+    button.className = "accordion";
+    button.innerHTML = title;
+    var panel = document.createElement("div");
+    panel.className = "panel";
+    panel.innerHTML = content;
+
+    // Add the button and panel to the container
+    var container = document.getElementById("panel-container");
+    container.appendChild(button);
+    container.appendChild(panel);
+
+    // Add the click event listener to the new button
+    button.addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+// Example usage of the addPanel function
+
+
+// addPanel("Section 2", "<p>I will type words here</p>");
+// addPanel("Section 3", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 4", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 5", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 6", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 7", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 8", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 9", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 10", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 11", "<p>Content for section 3 goes here.</p>");
+// addPanel("Section 12", "<p>Content for section 3 goes here.</p>");
